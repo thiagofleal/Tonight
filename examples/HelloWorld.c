90d5bec692b9074534aa3ce6cf3c52bf5f575534
thiagofleal <thiagofernandesleal@yahoo.com.br>
2019-11-04 20:08:13 -0300

    Add Examples: Hello World

---
+++ examples/HelloWorld.c
@@ -0,0 +1,17 @@
+#include <tonight.h>
+#include <Tonight/application.h>
+#include <Tonight/console.h>
+#include <Tonight/writer.h>
+
+int Main(string ARRAY args)
+{
+    object w = $Empty(object);
+
+    using(w $as new(Writer, Console.getStream()) $with delete)
+    {
+        $(w $as Writer).textln("Hello World!");
+    }
+
+    Console.getKey();
+    return ExitValue.Success;
+}
