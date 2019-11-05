563a5209ef380c5da367db01823c7e6cf759a4a9
thiagofleal <thiagofernandesleal@yahoo.com.br>
2019-11-05 14:47:59 -0300

    Add example: Average

---
+++ examples/average.c
@@ -0,0 +1,41 @@
+#include <tonight.h>
+#include <Tonight/exceptions.h>
+#include <Tonight/string.h>
+#include <Tonight/application.h>
+#include <Tonight/console.h>
+#include <Tonight/writer.h>
+#include <Tonight/reader.h>
+
+int Main(string ARRAY args)
+{
+    object w = NULL;
+    object r = NULL;
+
+    using(w $as new(Writer, Console.getStream()) $with delete)
+    {
+        using(r $as new(Reader, Console.getStream()) $with delete)
+        {
+            try
+            {
+                double n1, n2, average;
+
+                $(w $as Writer).text("Input the first value: ");
+                n1 = $(r $as Reader).nextDouble();
+                $(w $as Writer).text("Input the second value: ");
+                n2 = $(r $as Reader).nextDouble();
+
+                average = (n1 + n2) / 2.0;
+
+                $(w $as Writer).println("The average between ", $d(n1), " and ", $d(n2), " is ", $d(average), $end);
+                Console.getKey();
+            }
+            catch(GenericException)
+            {
+		$(w $as Writer).printargln(CurrentException.error(), CurrentException.message(), $end);
+		Console.getKey();
+            }
+        }
+    }
+
+    return ExitValue.Success;
+}
